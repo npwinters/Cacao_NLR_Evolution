10fb80101a477bd63baa78460c7df108f219155f
npwinters <winters.noahp@gmail.com>
2024-05-10 10:07:23 -0400

    Delete pseudogene_annot directory

--- pseudogene_annot/parse_pfam_hiConfPseudo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Noah Winters
-# JAN 2022
-# Parses high confidence pseudogene identifications from Pfam
-FILE=$1
-cat $FILE | grep -f nlr_pfam_id.txt | cut -f 1 | sed 's/_fr.*//g' | sort | uniq > ${FILE%_pfam.tsv}_cleaned_pfam.txt
--- pseudogene_annot/translate_pseudoGenes.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Noah Winters
-# JAN 2022
-# Translates annotated pseudogenes in all 6 frames
-FILE=$1
-cat $FILE | sed '/^#/d' | translate6frames.sh -in=stdin.fa  out=stdout.fa | sed 's/\*/X/g' | sed 's/\./X/g' | sed 's/ fr/_fr/g' > ${FILE}.faa
-
-
